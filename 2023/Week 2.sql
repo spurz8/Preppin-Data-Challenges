dcc1edf0c264809ca7da89664300db78476dd9ef
Serena Purslow <163403636+spurz8@users.noreply.github.com>
2024-11-01 15:55:12 +0000

    Update Week 2.sql

--- 2023/Week 2.sql
+++ 2023/Week 2.sql
@@ -1,3 +1,14 @@
+-- Requirements
+-- Input the data
+-- In the Transactions table, there is a Sort Code field which contains dashes. We need to remove these so just have a 6 digit string (hint)
+-- Use the SWIFT Bank Code lookup table to bring in additional information about the SWIFT code and Check Digits of the receiving bank account (hint)
+-- Add a field for the Country Code (hint)
+--     Hint: all these transactions take place in the UK so the Country Code should be GB
+-- Create the IBAN as above (hint)
+--     Hint: watch out for trying to combine sting fields with numeric fields - check data types
+-- Remove unnecessary fields (hint)
+-- Output the data
+
 -- View datasets
 
 SELECT *
