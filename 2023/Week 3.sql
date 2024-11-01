171ca76ddf6b706c9e9ea0fbd5426366c428913d
Serena Purslow <163403636+spurz8@users.noreply.github.com>
2024-11-01 15:56:22 +0000

    Update Week 3.sql

--- 2023/Week 3.sql
+++ 2023/Week 3.sql
@@ -1,3 +1,24 @@
+-- Requirements
+-- Input the data
+-- For the transactions file:
+-- Filter the transactions to just look at DSB (help)
+--    These will be transactions that contain DSB in the Transaction Code field
+-- Rename the values in the Online or In-person field, Online of the 1 values and In-Person for the 2 values
+-- Change the date to be the quarter (help)
+-- Sum the transaction values for each quarter and for each Type of Transaction (Online or In-Person) (help)
+
+-- For the targets file:
+-- Pivot the quarterly targets so we have a row for each Type of Transaction and each Quarter (help)
+-- Rename the fields
+-- Remove the 'Q' from the quarter field and make the data type numeric (help)
+
+-- Join the two datasets together (help)
+--   You may need more than one join clause!
+-- Remove unnecessary fields
+-- Calculate the Variance to Target for each row (help)
+-- Output the data
+
+
 --View the data
 
 SELECT * 
