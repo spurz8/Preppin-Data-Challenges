10cd78396b1c4abbecaaffca3fbbc17644c51714
Serena Purslow <163403636+spurz8@users.noreply.github.com>
2024-11-01 15:54:35 +0000

    Update Week 1.sql

--- 2023/Week 1.sql
+++ 2023/Week 1.sql
@@ -1,3 +1,16 @@
+-- Requirements
+-- Input the data (help)
+-- Split the Transaction Code to extract the letters at the start of the transaction code. These identify the bank who processes the transaction (help)
+-- Rename the new field with the Bank code 'Bank'. 
+-- Rename the values in the Online or In-person field, Online of the 1 values and In-Person for the 2 values. 
+-- Change the date to be the day of the week (help)
+-- Different levels of detail are required in the outputs. You will need to sum up the values of the transactions in three ways (help):
+--    1. Total Values of Transactions by each bank
+--    2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
+--    3. Total Values by Bank and Customer Code
+-- Output each data file (help)
+
+
 -- Start by viewing the data
 SELECT *
 FROM FROM PD2023_WK01;
