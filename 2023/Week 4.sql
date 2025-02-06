d1556b8528244d622cea52d4565ac85e468547ef
Serena Purslow <163403636+spurz8@users.noreply.github.com>
2025-02-06 16:01:42 +0000

    Update Week 4.sql

--- 2023/Week 4.sql
+++ 2023/Week 4.sql
@@ -1,1 +1,15 @@
---
+--Requirements
+-- Input the data
+-- We want to stack the tables on top of one another, since they have the same fields in each sheet. We can do this one of 2 ways (help):
+------ Drag each table into the canvas and use a union step to stack them on top of one another
+------ Use a wildcard union in the input step of one of the tables
+-- Some of the fields aren't matching up as we'd expect, due to differences in spelling. Merge these fields together
+-- Make a Joining Date field based on the Joining Day, Table Names and the year 2023
+-- Now we want to reshape our data so we have a field for each demographic, for each new customer (help)
+-- Make sure all the data types are correct for each field
+------ Remove duplicates (help)
+-- If a customer appears multiple times take their earliest joining date
+-- Output the data
+
+
+-- View the data
